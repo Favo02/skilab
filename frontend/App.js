d86e6d1e2ff7773171597b99e2309c4f95eac162
Matteo Zagheno <matteo2309@hotmail.com>
2023-08-04 18:21:11 +0200

    added button

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
+import * as DataAccess from './DataAccess.js'
+
 
 export default function App() {
+  //DataAccess.getSkiAreas().then((res) => console.log(res));
   return (
     <View style={styles.container}>
+      <Button
+        style={styles.btn}
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
       <MapView style={styles.map} />
     </View>
   );
@@ -13,6 +21,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  btn: {
+    height: '100px',
   },
   map: {
     width: '100%',
