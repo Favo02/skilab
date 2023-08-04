0f78f7b77d1acd763dabbd0ac9a7af0f9f7bf2f3
Matteo Zagheno <matteo2309@hotmail.com>
2023-08-04 22:50:37 +0200

    Added  getAccomodationNearPoint

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import Map from "./components/Map"
 import Home from "./components/Home"
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as DataAccess from './services/DataAccess.js';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  DataAccess.getAccomodationNearPoint(46.685657, 11.725011, 5).then((res) => res.forEach(e => {
+    console.log(e.Distance)
+  }));
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -14,13 +18,13 @@ export default function App() {
         <Stack.Screen
           name="Home"
           component={Home}
-          options={{title: 'Home'}}
+          options={{ title: 'Home' }}
         />
 
         <Stack.Screen
           name="Map"
           component={Map}
-          options={{title: 'map'}}
+          options={{ title: 'map' }}
         />
 
       </Stack.Navigator>
--- frontend/services/DataAccess.js
+++ frontend/services/DataAccess.js
@@ -29,4 +29,46 @@ export async function getSkiAreas() {
         });
     });
     return areas
-} 
+}
+
+
+//https://tourism.opendatahub.com/v1/Accommodation?pagenumber=2&pagesize=100&roominfo=1-18,18&bokfilter=hgv&msssource=sinfo&availabilitychecklanguage=en&detail=0&latitude=46.685657&longitude=11.725011&removenullvalues=false
+
+export async function getAccomodationNearPoint(latitude, longitude, number) {
+    const radius = 10000;
+    const pageSize = 1000;
+    const pageNumber = 1;
+    let res = await fetch("https://tourism.opendatahub.com/v1/Accommodation?pagenumber=" + pageNumber + "&pagesize=" + pageSize + "&roominfo=1-18,18&bokfilter=hgv&msssource=sinfo&availabilitychecklanguage=en&detail=0&latitude=" + latitude + "&longitude=" + longitude + "&removenullvalues=false&radius=" + radius);
+    res = await res.json();
+    //console.log(res);
+    let accomodations = [];
+    res.Items.forEach(e => {
+        let distance = (e.GpsPoints.position.Longitude - longitude) * (e.GpsPoints.position.Longitude - longitude) + (e.GpsPoints.position.Latitude - latitude) * (e.GpsPoints.position.Latitude - latitude);
+        accomodations.push({
+            Id: e.Id,
+            Distance: distance,
+            AccoType: e.AccoType,
+            Altitude: e.Altitude,
+            AltitudeTo: e.AltitudeTo,
+            AltitudeUnitofMeasure: e.AltitudeUnitofMeasure,
+            Detail: e.Detail,
+            GpsPoints: {
+                position: {
+                    Gpstype: e.GpsPoints.position.Gpstype,
+                    Latitude: e.GpsPoints.position.Longitude, //Inverted
+                    Longitude: e.GpsPoints.position.Latitude,
+                    AltitudeUnitofMeasure: e.AltitudeUnitofMeasure,
+                }
+            },
+            IsCamping: e.IsCamping,
+            AccoDetail: e.AccoDetail,
+            AccoTypeId: e.AccoTypeId,
+            ImageGallery: e.ImageGallery,
+            LocationInfo: e.LocationInfo,
+        });
+    });
+    accomodations.sort((a, b) => a.Distance - b.Distance);
+    accomodations = accomodations.slice(0, number);
+    return accomodations
+}
+
