a61c82f20fc35abf43b0a97af37c9bf16641b705
Matteo Zagheno <matteo2309@hotmail.com>
2023-08-05 10:57:39 +0200

    Added default radius, distance function  and parking fix

--- frontend/services/DataAccess.js
+++ frontend/services/DataAccess.js
@@ -1,4 +1,7 @@
 // get ski areas
+
+export const defaultRadius = 10000; //meters
+
 export async function getSkiAreas() {
   let res = await fetch("https://tourism.opendatahub.com/v1/SkiArea")
   res = await res.json()
@@ -14,7 +17,7 @@ export async function getSkiAreas() {
 }
 
 // get accomodations near something
-export async function getAccomodationNearPoint(latitude, longitude) {
+export async function getAccomodationNearPoint(latitude, longitude, radius = defaultRadius) {
   const pageSize = 1000
   const pageNumber = 1
 
@@ -32,13 +35,11 @@ export async function getAccomodationNearPoint(latitude, longitude) {
   )
   res = await res.json()
 
+  console.log("got accomodations");
+  console.log("number of accomodations:", res.Items.length)
   let accomodations = []
   res.Items.forEach((e) => {
-    let distance =
-      (e.GpsPoints.position.Longitude - longitude) *
-      (e.GpsPoints.position.Longitude - longitude) +
-      (e.GpsPoints.position.Latitude - latitude) *
-      (e.GpsPoints.position.Latitude - latitude)
+    let distance = computeDistanceFromLongLat(e.GpsPoints.position.Longitude, longitude, e.GpsPoints.position.Latitude, latitude);
     accomodations.push({
       Id: e.Id,
       Distance: distance,
@@ -63,15 +64,15 @@ export async function getAccomodationNearPoint(latitude, longitude) {
     })
   })
   accomodations.sort((a, b) => a.Distance - b.Distance)
+  console.log("done sorting accomodations");
   return accomodations
 }
 
 
 //get parking spots in a radius
-export async function getParkingNearPoint(latitude, longitude, radius = 10000) {
-  const pageSize = 1000
+export async function getParkingNearPoint(latitude, longitude, radius = defaultRadius) {
+  const pageSize = 10000
   const pageNumber = 1
-
   let res = await fetch(
     "https://tourism.opendatahub.com/v1/ODHActivityPoi?" +
     "pagenumber=" + pageNumber +
@@ -83,14 +84,25 @@ export async function getParkingNearPoint(latitude, longitude, radius = 10000) {
   )
   res = await res.json()
 
+  console.log("got parkings");
+  console.log("number of parkings:", res.Items.length)
   let parkings = []
-  res.forEach(e => {
-    if (e.Details.en.ParkingInfo) {
+  res.Items.forEach(e => {
+    let distance = computeDistanceFromLongLat(e.GpsPoints.position.Longitude, longitude, e.GpsPoints.position.Latitude, latitude);
+    if (e.Details?.en?.ParkingInfo) {
       parkings.push({
         Title: e.Detail.Title,
+        Distance: distance,
         GpsInfo: e.GpsInfo[0],
       });
     }
   });
+  console.log("final number of parkings:", res.Items.length);
+  parkings.sort((a, b) => a.Distance - b.Distance)
+  console.log("done sorting parkings");
   return parkings
+}
+
+function computeDistanceFromLongLat(lon0, lon1, lat0, lat1) {
+  return (lon1 - lon0) * (lon1 - lon0) + (lat1 - lat0) * (lat1 - lat0);
 }
