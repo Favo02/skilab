d3343cf9a9a4979500a43cc9c4f024bd591c3b44
Luca Favini <luca.favini.02@gmail.com>
2023-08-05 02:59:30 +0200

    add accomodations near sky areas in Map

--- frontend/components/Map.js
+++ frontend/components/Map.js
@@ -1,44 +1,93 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import MapView, { Marker, Callout } from "react-native-maps"
 import { StyleSheet, View, Text } from "react-native"
-import { getSkiAreas } from "../services/DataAccess.js"
+import {
+  getSkiAreas,
+  getAccomodationNearPoint,
+} from "../services/DataAccess.js"
+
+const INITIAL_REGION = {
+  latitude: 46.6561,
+  longitude: 11.1919,
+  latitudeDelta: 2,
+  longitudeDelta: 2,
+}
 
 export default function Map() {
-  const [data, setData] = useState([])
 
+  const mapRef = useRef(null)
+
+  const [skyAreas, setSkyAreas] = useState([])
+  const [accomodations, setAccomodations] = useState([])
+
+  // fetch skyAreas
   useEffect(() => {
     const getData = async () => {
-      setData(await getSkiAreas())
+      setSkyAreas(await getSkiAreas())
     }
 
     getData()
   }, [])
 
+  // getch accomodations around close to something
+  const getNewData = async (lat, lon, range) => {
+    setAccomodations([])
+    setAccomodations(await getAccomodationNearPoint(lat, lon, range))
+  }
+
+  const handleClick = async (lat, lon) => {
+    if (mapRef.current) {
+      const newRegion = {
+        latitude: lat,
+        longitude: lon,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      }
+
+      mapRef.current.animateToRegion(newRegion, 1000)
+    }
+
+    getNewData(lat, lon, 5)
+  }
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={{
-          longitude: 11.5,
-          latitude: 46.5,
-          latitudeDelta: 1,
-          longitudeDelta: 1,
-        }}
+        initialRegion={INITIAL_REGION}
+        ref={mapRef}
       >
-        {data.map((p, i) => (
+        {/* Sky areas markers */}
+        {skyAreas.map((p, i) => (
           <Marker
             title={p.ShortName}
             key={i}
             coordinate={p.GpsPoints.position}
-            strokeWidth={2}
-            strokeColor="#f00"
+            pinColor="#f00"
+            onPress={() =>
+              handleClick(
+                p.GpsPoints.position.latitude,
+                p.GpsPoints.position.longitude
+              )
+            }
           >
             <Callout>
               <View>
+                <Text>{JSON.stringify(p.GpsPoints.position)}</Text>
                 <Text>{JSON.stringify(p.Detail)}</Text>
               </View>
             </Callout>
           </Marker>
+        ))}
+
+        {/* Accomodation markers */}
+        {accomodations.map((p, i) => (
+          <Marker
+            title={p.ShortName}
+            key={i}
+            coordinate={p.GpsPoints.position}
+            pinColor="#00f"
+          />
         ))}
       </MapView>
     </View>
